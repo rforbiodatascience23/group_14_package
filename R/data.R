756aba069b6334c95ae6623de8ecd1330b483572
Navaneeth-K-P <me1180630@iitd.ac.in>
2023-10-24 10:43:48 +0200

    object created

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Codon Table
+#'
+#' codon_table: After the transcription process, you have mRNA sequence and we use this mRNA sequence to produce a functional protein (aminoacid sequence).
+#' To produce this amino acid sequence, we use codon (which are basically 3 bases of mRNA starting from position one, start codon).
+#' We use this codon table to produce our amino acid sequence. For each codon we have our speicif amino acid with its own biochemical properties.
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"

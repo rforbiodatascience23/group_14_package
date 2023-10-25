9e453b3cd8d19f3b9ebc532dac32e0329d7e0b09
emmasena <s193588@dtu.dk>
2023-10-25 22:23:41 +0200

    function defined

---
+++ R/assemble.R
@@ -0,0 +1,13 @@
+#' Function 4: Assemble sequences
+#'
+#' @param codons
+#'
+#' @return codon values
+#' @export
+#'
+#' @examples
+#' assemble("CCG")
+assemble <- function(codons){
+  idenfity <- paste0(codon_table[codons], collapse = "")
+  return(identify)
+}

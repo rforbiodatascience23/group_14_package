5d2cbff006e721f59775f709ac11767c305546ae
Navaneeth-K-P <me1180630@iitd.ac.in>
2023-10-26 22:35:43 +0200

    vignette and tests updated

--- tests/testthat/test-dna_seq.R
+++ tests/testthat/test-dna_seq.R
@@ -1,7 +1,4 @@
 test_that("random dna sequence is generated of length 4", {
-  expect_equal(dna_seq(4), "ATCG")
+  expect_equal(nchar(dna_seq(4)), 4)
 })
 
-test_that("random dna sequence is generated of length 8", {
-  expect_equal(dna_to_rna(8), "ATCGGGGA")
-})
